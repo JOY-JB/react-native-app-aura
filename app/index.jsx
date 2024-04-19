ace0058a3c896d2c6fecdfe260db2178d3bb837f
JOY-JB <joybarua0058@gmail.com>
2024-04-19 21:47:01 +0600

    tabbar design done

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-pblack">Aora</Text>
+      <Link href="/home">Home</Link>
       <StatusBar style="auto" />
     </View>
   );
